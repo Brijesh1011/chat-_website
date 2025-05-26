385528e759f77b450ca921e6e54def5c1d299994
Brijesh1011 <thebrijesh1011@gmail.com>
2025-05-26 18:12:56 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,10 @@ app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({
+  origin: process.env.FRONT_END_URI,
+  credentials: true
+}));
 
 app.use("/api/auth", router)
 app.use("/api/messages", messageroute)
