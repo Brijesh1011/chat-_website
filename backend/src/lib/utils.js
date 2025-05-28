83fe1ca61b4f4bff67580f3199eb866175ff90cf
Brijesh1011 <brijeshparmar1011@gmail.com>
2025-05-28 22:09:06 +0530

    fixed

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -25,8 +25,9 @@ export const generateToken = (userId, res) => {
  res.cookie("jwt", token, {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
-  secure: process.env.NODE_ENV !== "development",
+  sameSite: "None",
+secure: true
+
 });
 
 
