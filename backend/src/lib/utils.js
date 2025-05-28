57a7ac09f73d8300ee6c891b8acbf4a26e166d69
Brijesh1011 <brijeshparmar1011@gmail.com>
2025-05-28 21:51:49 +0530

    fixed

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -22,12 +22,13 @@ export const generateToken = (userId, res) => {
     expiresIn: "7d",
   });
 
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    httpOnly: true,
-    sameSite: "None",               // ✅ Fix for cross-site
-    secure: process.env.NODE_ENV !== "development", // ✅ Required with "None"
-  });
+ res.cookie("jwt", token, {
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+  secure: process.env.NODE_ENV !== "development",
+});
+
 
   return token;
 };
